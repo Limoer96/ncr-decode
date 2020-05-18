55b7d916b6420e827d211f831fb16ee0d9bf61ab
Limoer96 <myt19960503@outlook.com>
2020-05-18 16:16:55 +0800

    [*]简化api命名，修改cli输出结果显示

--- src/client.js
+++ src/client.js
@@ -1,44 +1,61 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
-const { dfmDecode } = require('./index');
+const readline = require('readline')
+const chalk = require('chalk')
+const { dfmDecode } = require('./index')
 
 function _findIndex(args) {
-  let expected = ['-i', '--inline'];
+  let expected = ['-i', '--inline']
   for (let i = 0; i < args.length; i += 1) {
     if (expected.indexOf(args[i]) > -1) {
-      return i;
+      return i
     }
   }
-  return -1;
+  return -1
+}
+/**
+ * 打印输出结果
+ * @param {*} transformedContent 解码后的内容
+ * @param {*} originContent 初始内容
+ */
+function _formatOutput(transformedContent, originContent) {
+  if (!originContent || !transformedContent) {
+    return
+  }
+  // 无法decode的内容
+  if (transformedContent === originContent) {
+    console.log(chalk.red('[!invalid] '), chalk.gray(transformedContent))
+    return
+  }
+  console.log(chalk.green(transformedContent))
 }
 
-const args = process.argv.slice(2);
-const index = _findIndex(args);
+const args = process.argv.slice(2)
+const index = _findIndex(args)
 if (index > -1) {
-  let params = [...args];
-  params.splice(index, 1);
-  console.log(dfmDecode(params[0]));
+  let params = [...args]
+  params.splice(index, 1)
+  const result = dfmDecode(params[0])
+  _formatOutput(result, params[0])
 } else {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: '-> ',
-  });
-
-  console.log('enter `.exit` to exit.');
-  rl.prompt();
-  
-  rl.on('line', (line) => {
-    if(line === '.exit') {
-      process.exit(0);
-    }
-    const result = dfmDecode(line);
-    console.log(result);
-    rl.prompt();
+    prompt: '> ',
   })
-    .on('close', () => {
-      process.exit(0);
-    });
-}
 
+  console.log(chalk.bgCyan('enter `.exit` to exit.'))
+  rl.prompt()
+
+  rl.on('line', (line) => {
+    if (line === '.exit') {
+      process.exit(0)
+    }
+    const result = dfmDecode(line)
+    // console.log(result)
+    _formatOutput(result, line)
+    rl.prompt()
+  }).on('close', () => {
+    process.exit(0)
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,61 +1,67 @@
 /**
  * numerric character reference(NCR) to raw chinese
- * @param {string} str 
+ * @param {string} str
  */
 function ncrDecode(str) {
-  str = str.replace(/(&#)(\d{1,6});/gi,function($0){ 
-		return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g,"$2"))); 
-  });
-  str = str.replace(/(&#x)(\w{1,4});/gi,function($0){ 
-		return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g,"$2"),16)); 
-	});
-  return str;
+  str = str.replace(/(&#)(\d{1,6});/gi, function ($0) {
+    return String.fromCharCode(
+      parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, '$2'))
+    )
+  })
+  str = str.replace(/(&#x)(\w{1,4});/gi, function ($0) {
+    return String.fromCharCode(
+      parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, '$2'), 16)
+    )
+  })
+  return str
 }
 /**
  * special encode string to NCR
  * @param {string} str
  */
 function str2NCR(str) {
-  if (!typeof str === 'string') return '';
-  let reg = /(#x?)/g;
-  if(!reg.test(str)) return str;
-  str = str.replace(reg, ';&$&');
-  str = str.slice(1);
-  str += ';';
-  return str;
+  if (!typeof str === 'string') return ''
+  let reg = /(#x?)/g
+  if (!reg.test(str)) return str
+  str = str.replace(reg, ';&$&')
+  str = str.slice(1)
+  str += ';'
+  return str
 }
 /**
  * string to NCR string
  * @param {string} str
  * @param {number} radix 10 or 16 default is 10
- * @returns {string} NRC string 
+ * @returns {string} NRC string
  */
 function ncrEncode(str, radix = 10) {
-  if (!str || typeof str !=='string') return '';
+  if (!str || typeof str !== 'string') return ''
   if ([10, 16].indexOf(radix) === -1) {
-    throw new Error(`Unsupported radix ${radix}, use 10 or 16 instead`);
+    throw new Error(`Unsupported radix ${radix}, use 10 or 16 instead`)
   }
-  let result = '';
+  let result = ''
   for (let char of str) {
-    let codePoint = char.codePointAt().toString(radix);
-    result += `&#${radix === 16 ? 'x': ''}${codePoint};`;
+    let codePoint = char.codePointAt().toString(radix)
+    result += `&#${radix === 16 ? 'x' : ''}${codePoint};`
   }
-  return result;
+  return result
 }
 
 function checkNCR(str) {
-  let reg = /&#x?/;
-  return reg.test(str);
+  let reg = /&#x?/
+  return reg.test(str)
 }
 
 /**
  * decode delphi .dfm file encoded chinese
- * @param {string} str 
+ * @param {string} str
  */
 function dfmDecode(str) {
-  return checkNCR(str) ? ncrDecode(str) : ncrDecode(str2NCR(str));
+  return checkNCR(str) ? ncrDecode(str) : ncrDecode(str2NCR(str))
 }
 
-exports.ncrDecode = ncrDecode;
-exports.ncrEncode = ncrEncode;
-exports.dfmDecode =dfmDecode;
+exports.ncrDecode = ncrDecode
+exports.decode = ncrDecode
+exports.ncrEncode = ncrEncode
+exports.encode = ncrEncode
+exports.dfmDecode = dfmDecode
